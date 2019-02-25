879b11159f69f28225fe3b76f636a1d9079487b2
Michael Banks <michaelbanks@Michaels-MacBook-Pro-2.local>
2019-02-25 17:03:37 -0600

    DB connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import routes from './routes';
-import mongoose from 'mongoose';
-import uri from '../db/config';
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://mbanks:12345@rmm-api-0m2qy.mongodb.net/test?retryWrites=true";
 
 const app = express();
 const PORT = 3000;
@@ -9,4 +9,12 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
     mongoose.connect(uri, {useNewUrlParser: true});
+});
+const db = mongoose.connection;
+
+db.on('error', err => {
+  console.error(`Error while connecting to DB: ${err.message}`);
+});
+db.once('open', () => {
+  console.log('DB connected successfully!');
 });
