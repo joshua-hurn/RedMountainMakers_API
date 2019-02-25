294b75d9f2e3836733bcf1f6f09357d7bc7c5731
joshua-hurn <joshua.hurn@hotmail.com>
2019-02-25 16:44:13 -0600

    merging

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 const app = express();
 const PORT = 3000;
+
+app.get('/', (req, res) => {res.send('hello')});
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
