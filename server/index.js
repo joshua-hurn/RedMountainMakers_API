8b3655768a16fbeec5be51b9e4bc2b7abf038fda
Michael Banks <michaelbanks@Michaels-MacBook-Pro-2.local>
2019-03-02 12:08:38 -0600

    auth working

--- server/index.js
+++ server/index.js
@@ -2,20 +2,29 @@ require('dotenv').config()
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
 const uri = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect(uri, {
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
+
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use(express.json());
 app.use('/api', routes);
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
-    mongoose.connect(uri, {
-      useCreateIndex: true,
-      useNewUrlParser: true
-    });
 });
 const db = mongoose.connection;
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,87 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+// Models
 const Users = require('../../db/models').Users;
 const Classes = require('../../db/models').Classes;
+
+// Login Page
+router.get('/login', (req, res) => {
+  res.send('login');
+});
+
+// Register Page
+router.get('/register', (req, res) => {
+  res.send('register');
+});
+
+// Register
+router.post('/register', (req, res) => {
+  const { email, firstName, lastName, password, password2 } = req.body;
+  let errors = [];
+
+  // Check required Fields
+  if (!email || !firstName || !lastName || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields.' });
+  }
+  // Check passwords match
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match.' });
+  }
+  // Check password length
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.send({ errors });
+  } else {
+    Users.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email is already registered.' });
+        res.send(errors);
+      } else {
+        const newUser = new Users({
+          email,
+          firstName,
+          lastName,
+          password
+        });
+        // Hash Password
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            // newUser.salt = salt;
+            newUser
+              .save()
+              .then(user => {
+                res.redirect('/api/login');
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
+
+// // Login
+// router.post('/login', (req, res, next) => {
+//   passport.authenticate('local', {
+//     successRedirect: '/dashboard',
+//     failureRedirect: '/users/login',
+//     failureFlash: true
+//   })(req, res, next);
+// });
+
+// // Logout
+// router.get('/logout', (req, res) => {
+//   req.logout();
+//   req.flash('success_msg', 'You are logged out');
+//   res.redirect('/users/login');
+// });
 
 // User routes // Works!
 router.get('/users', (req, res, next) => {
@@ -38,7 +118,7 @@ router.post('/users', (req, res, next) => {
 
 // Update User // Works!
 router.put('/users/:id', (req, res, next) => {
-  Users.findOneAndUpdate({_id: {$eq: req.params.id}}, {$set: req.body}, 
+  Users.findOneAndUpdate({ _id: { $eq: req.params.id } }, { $set: req.body },
     (err) => {
       if (err) return next(err);
       res.send('User updated.');
@@ -47,7 +127,7 @@ router.put('/users/:id', (req, res, next) => {
 
 // Delete User // Works!
 router.delete('/users/:id', (req, res, next) => {
-  Users.findOneAndDelete({_id: {$eq: req.params.id}}, 
+  Users.findOneAndDelete({ _id: { $eq: req.params.id } },
     (err) => {
       if (err) return next(err);
       res.send('User Deleted.');
